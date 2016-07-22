fdaa7f20edc2584c6fc6100911d5ba7c232aabda
Tom Beeman <tbeeman93@gmail.com>
2016-07-21 22:44:06 -0700

    Added basic crawling. Makes character tiny. Speed unchanged. Press C to toggle.

---
+++ Assets/Scripts/CrawlingController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrawlingController : MonoBehaviour {
+
+	private bool isStanding = true;
+	private float standingHeight;
+	private float proneHeight;
+
+	// Use this for initialization
+	void Start () {
+		standingHeight = transform.localScale.y;
+		proneHeight = 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.C) && isStanding) {
+			transform.localScale = new Vector3 (transform.localScale.x, proneHeight, transform.localScale.z);
+			isStanding = false;
+			print ("Crouch");
+		}
+		else if (Input.GetKeyDown (KeyCode.C) && !isStanding) 
+		{
+			transform.localScale = new Vector3 (transform.localScale.x, proneHeight, transform.localScale.z);
+			isStanding = true;
+			print ("stand up");
+		}
+	}
+}
