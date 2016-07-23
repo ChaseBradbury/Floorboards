fe9f9b5192bbde0c4ed915b6c99a30333ac19848
Chase Bradbury <chasebradbury@gmail.com>
2016-07-22 21:49:19 -0700

    Shit Particles Less Shit
    But still shit

---
+++ Assets/DustParticleScript2.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DustParticleScript2 : MonoBehaviour
+{
+    public Vector2 boundsX;
+    public Vector2 boundsY;
+    public Vector2 boundsZ;
+
+    public Vector3 velocity;
+    private float velRange;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        velRange = 0.001F;
+        velocity = new Vector3(Random.Range(-velRange, velRange), Random.Range(-velRange, velRange), Random.Range(-velRange, velRange));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x < boundsX.x)
+        {
+            this.transform.position = new Vector3(boundsX.x + 0.01F, this.transform.position.y, this.transform.position.z);
+            velocity = new Vector3(Random.Range(-velRange, velRange), Random.Range(-velRange, velRange), Random.Range(-velRange, velRange));
+        }
+        if (this.transform.position.x > boundsX.y)
+        {
+            this.transform.position = new Vector3(boundsX.y - 0.01F, this.transform.position.y, this.transform.position.z);
+            velocity = new Vector3(Random.Range(-velRange, velRange), Random.Range(-velRange, velRange), Random.Range(-velRange, velRange));
+        }
+        if (this.transform.position.y < boundsY.x)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, boundsY.x + 0.01F, this.transform.position.z);
+            velocity = new Vector3(Random.Range(-velRange, velRange), Random.Range(-velRange, velRange), Random.Range(-velRange, velRange));
+        }
+        if (this.transform.position.y > boundsY.y)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, boundsY.y - 0.01F, this.transform.position.z);
+            velocity = new Vector3(Random.Range(-velRange, velRange), Random.Range(-velRange, velRange), Random.Range(-velRange, velRange));
+        }
+        if (this.transform.position.z < boundsZ.x)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, boundsZ.x + 0.01F);
+            velocity = new Vector3(Random.Range(-velRange, velRange), Random.Range(-velRange, velRange), Random.Range(-velRange, velRange));
+        }
+        if (this.transform.position.z > boundsZ.y)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, boundsZ.x + 0.01F);
+            velocity = new Vector3(Random.Range(-velRange, velRange), Random.Range(-velRange, velRange), Random.Range(-velRange, velRange));
+        }
+        this.transform.position += velocity;
+        transform.rotation = Quaternion.LookRotation(-Camera.main.transform.forward);
+    }
+
+    public void Reset(Vector3 position)
+    {
+        this.transform.position = position;
+    }
+}
---
+++ Assets/DustParticleSystemScript2.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DustParticleSystemScript2 : MonoBehaviour
+{
+
+    public Transform particle;
+    public int numParticles;
+    public Vector3 startVelocity;
+    public Vector3 startScale;
+    public Color startColor;
+    
+    private DustParticleScript2[] particles;
+
+    // Use this for initialization
+    void Start()
+    {
+        particles = new DustParticleScript2[numParticles];
+        float newXPos;
+        float newYPos;
+        float newZPos;
+        Transform newParticle;
+        DustParticleScript2 newParticleScript;
+        int i;
+        for (i = 0; i < numParticles; i++)
+        {
+            newXPos = Random.Range(this.transform.position.x - this.transform.localScale.x, this.transform.position.x + this.transform.localScale.x);
+            newYPos = Random.Range(this.transform.position.y - this.transform.localScale.y, this.transform.position.y + this.transform.localScale.y);
+            newZPos = Random.Range(this.transform.position.z - this.transform.localScale.z, this.transform.position.z + this.transform.localScale.z);
+            newParticle = Transform.Instantiate(particle, new Vector3(newXPos, newYPos, newZPos), Quaternion.identity) as Transform;
+            newParticle.localScale = startScale;
+            newParticle.GetComponent<SpriteRenderer>().color = startColor;
+            newParticleScript = newParticle.GetComponent<DustParticleScript2>();
+            newParticleScript.boundsX = new Vector2(this.transform.position.x - this.transform.localScale.x, this.transform.position.x + this.transform.localScale.x);
+            newParticleScript.boundsY = new Vector2(this.transform.position.y - this.transform.localScale.y, this.transform.position.y + this.transform.localScale.y);
+            newParticleScript.boundsZ = new Vector2(this.transform.position.z - this.transform.localScale.z, this.transform.position.z + this.transform.localScale.z);
+            particles[i] = newParticleScript;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
